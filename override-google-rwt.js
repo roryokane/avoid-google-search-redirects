7c85dbac1e8f9207ad3c85ad208ae3875e1d8d89
Adrien Dorsaz <adrien@adorsaz.ch>
2017-09-07 11:45:17 +0200

    override-google-rwt: override only if rwt exists

--- override-google-rwt.js
+++ override-google-rwt.js
@@ -19,6 +19,7 @@
 
 currentURL = new URL(window.location.href)
 
-if (currentURL.hostname.match(/\.google\.[^\.]+$/)) {
+if (window.wrappedJSObject.rwt
+    && currentURL.hostname.match(/\.google\.[^\.]+$/)) {
     window.wrappedJSObject.rwt = exportFunction(function() { }, window);
 }
