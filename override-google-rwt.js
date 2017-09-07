1b5e6c3df9be9ce3a23d880a94d294c972853904
Adrien Dorsaz <adrien@adorsaz.ch>
2017-09-07 09:58:00 +0200

    Apply GPLv3 License

--- override-google-rwt.js
+++ override-google-rwt.js
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Avoid Google Search redirects.
+ *
+ * Copyright ⓒ 2017 Adrien Dorsaz <adrien@adorsaz.ch>
+ *
+ * Avoid Google Search redirects is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Avoid Google Search redirects is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Avoid Google Search redirects.If not, see <http://www.gnu.org/licenses/>.
+ */
+
 currentURL = new URL(window.location.href)
 
 if (currentURL.hostname.match(/\.google\.[^\.]+$/)) {
